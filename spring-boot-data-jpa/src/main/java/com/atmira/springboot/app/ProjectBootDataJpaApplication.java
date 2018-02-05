d308e0299a3829551cc57808fe2acff70a8b10f5
Jose Marcos <jose.marcos@atmira.com>
2018-02-05 16:39:16 +0100

    NUevo proyecto

---
+++ spring-boot-data-jpa/src/main/java/com/atmira/springboot/app/ProjectBootDataJpaApplication.java
@@ -0,0 +1,12 @@
+package com.atmira.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBootDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBootDataJpaApplication.class, args);
+	}
+}
