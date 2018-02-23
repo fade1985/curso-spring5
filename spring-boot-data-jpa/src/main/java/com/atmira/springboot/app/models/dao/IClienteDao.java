3f75d8c1abe03e598f8d3e32971394d61b1793b1
Jose Marcos <jose.marcos@atmira.com>
2018-02-23 14:15:18 +0100

    .

--- spring-boot-data-jpa/src/main/java/com/atmira/springboot/app/models/dao/IClienteDao.java
+++ spring-boot-data-jpa/src/main/java/com/atmira/springboot/app/models/dao/IClienteDao.java
@@ -7,7 +7,7 @@ import com.atmira.springboot.app.models.entity.Cliente;
 
 public interface IClienteDao extends PagingAndSortingRepository<Cliente, Long> {
     
-    @Query("select c from Cliente c join fetch c.facturas f where c.id = ?1")
+    @Query("select c from Cliente c left join fetch c.facturas f where c.id = ?1")
     public Cliente fetchByIdWithFacturas(
         Long id);
 }
