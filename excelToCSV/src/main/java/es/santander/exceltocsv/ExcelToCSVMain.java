bd3f384440805a6be8d5caefbf01dcde836867e9
DESKTOP-FUEEH17\jmarc <jose.marcos@atmira.com>
2018-03-06 00:14:44 +0100

    no message

---
+++ excelToCSV/src/main/java/es/santander/exceltocsv/ExcelToCSVMain.java
@@ -0,0 +1,24 @@
+package es.santander.exceltocsv;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.apache.log4j.BasicConfigurator;
+
+import es.santander.exceltocsv.service.ExcelToCSVService;
+import es.santander.exceltocsv.service.impl.ExcelToCSVServiceImpl;
+
+public class ExcelToCSVMain {
+    
+    public static void main(
+        String[] args) throws URISyntaxException, InterruptedException, IOException{
+        
+        BasicConfigurator.configure();
+        
+        ExcelToCSVService service = new ExcelToCSVServiceImpl();
+        
+        // service.processFile(args[0]);
+        service.testSelenium("http://www.google.es");
+    }
+    
+}
---
+++ excelToCSV/src/main/java/es/santander/exceltocsv/service/ExcelToCSVService.java
@@ -0,0 +1,14 @@
+package es.santander.exceltocsv.service;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+public interface ExcelToCSVService {
+    
+    void processFile(
+        final String documentPath);
+    
+    void testSelenium(
+        final String url) throws MalformedURLException, URISyntaxException, InterruptedException, IOException;
+}
